ee7206f4a47a2e69596497595a9cc3e0a20bac88
SAHITHI2932 <72816042+SAHITHI2932@users.noreply.github.com>
2021-05-28 21:52:05 +0530

    Add files via upload

---
+++ part1/1.6/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const s1=()=>{setGood(good+1)}
+  const s2=()=>{setNeutral(neutral+1)}
+  const s3=()=>{setBad(bad+1)}
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <p>
+        <button onClick={s1}>good</button><button onClick={s2}>neutral</button><button onClick={s3}>bad</button>
+      </p>
+      <h2>Staistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  )
+}
+
+export default App
